90c4d266b1612861e576df63e71bfd3e42e20b04
MarcosVel <marcos.veloso@sempreceub.com>
2022-09-07 01:08:46 -0300

    feat: styles signIn/signUp

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ActivityIndicator, Text, View } from 'react-native';
+import { ActivityIndicator, View } from 'react-native';
 import AppRoutes from './app.routes';
 import AuthRoutes from './auth.routes';
 
@@ -7,13 +7,21 @@ function Routes() {
   const signed = false;
   const loading = false;
 
-  if (loading) {
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  return loading ? (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#36393f',
+      }}>
       <ActivityIndicator size={50} color="#e52246" />
-    </View>;
-  }
-
-  return signed ? <AppRoutes /> : <AuthRoutes />;
+    </View>
+  ) : signed ? (
+    <AppRoutes />
+  ) : (
+    <AuthRoutes />
+  );
 }
 
 export default Routes;
