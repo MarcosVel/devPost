13d7329b51f15d2f50c371f099f0076ff2e1b6a3
MarcosVel <marcos.veloso@sempreceub.com>
2022-09-08 23:04:28 -0300

    feat: add function to create account

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,3 +1,5 @@
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 import React, { createContext, useState } from 'react';
 
 export const AuthContext = createContext({});
@@ -5,8 +7,35 @@ export const AuthContext = createContext({});
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
 
+  async function signUp(name, email, password) {
+    await auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(async value => {
+        let uid = value.user.uid;
+        await firestore()
+          .collection('users')
+          .doc(uid)
+          .set({
+            name: name,
+            createdAt: new Date(),
+          })
+          .then(() => {
+            let data = {
+              uid: uid,
+              name: name,
+              email: value.user.email,
+            };
+
+            setUser(data);
+          });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   return (
-    <AuthContext.Provider value={{ signed: !!user }}>
+    <AuthContext.Provider value={{ signed: !!user, signUp }}>
       {children}
     </AuthContext.Provider>
   );
