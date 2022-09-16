fef0f2853743eca814b0baf7bb1deb9f3791f99c
MarcosVel <marcos.veloso@sempreceub.com>
2022-09-15 23:43:39 -0300

    feat: created header component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { Text } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
+import Header from '../../components/Header';
 import { ButtonPost, Container } from './styles';
 
 function Home() {
@@ -9,7 +10,7 @@ function Home() {
 
   return (
     <Container>
-      <Text>Home</Text>
+      <Header />
 
       <ButtonPost onPress={() => navigation.navigate('NewPost')}>
         <Feather name="edit-2" color="#fff" size={25} />
