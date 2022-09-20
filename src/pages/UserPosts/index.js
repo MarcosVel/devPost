3eece6a3df28c2b720c8425f20c38c8631ac89f6
MarcosVel <marcos.veloso@sempreceub.com>
2022-09-19 22:35:53 -0300

    feat: getting users posts

--- src/pages/UserPosts/index.js
+++ src/pages/UserPosts/index.js
@@ -1,17 +1,53 @@
-import { useNavigation, useRoute } from '@react-navigation/native';
-import React, { useLayoutEffect } from 'react';
+import {
+  useFocusEffect,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
+import React, { useCallback, useLayoutEffect, useState } from 'react';
 import { Text, View } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 function UserPosts() {
   const route = useRoute();
   const navigation = useNavigation();
   const userName = route.params.title;
 
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: userName,
     });
   }, [navigation, userName]);
+
+  useFocusEffect(
+    useCallback(() => {
+      let isActive = true;
+
+      firestore()
+        .collection('posts')
+        .where('userId', '==', route.params.userId)
+        .orderBy('created', 'desc')
+        .get()
+        .then(snapshot => {
+          const postList = [];
+          snapshot.docs.map(doc => {
+            postList.push({ ...doc.data(), id: doc.id });
+          });
+
+          if (isActive) {
+            setPosts(postList);
+            console.log(postList);
+            setLoading(false);
+          }
+        });
+
+      return () => {
+        isActive = false;
+      };
+    }, []),
+  );
 
   return (
     <View>
