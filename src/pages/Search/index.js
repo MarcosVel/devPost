41c93a67aca4c6410c11608c61637b11eb83b50b
MarcosVel <marcos.veloso@sempreceub.com>
2022-09-22 00:47:24 -0300

    feat: setting list of users

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,10 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Keyboard, TouchableWithoutFeedback } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 import { Container, Input, InputArea, List } from './styles';
+import firestore from '@react-native-firebase/firestore';
 
 const Search = () => {
   const [input, setInput] = useState('');
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    if (input === '' || input === undefined) {
+      setUsers([]);
+      return;
+    }
+
+    const subscriber = firestore()
+      .collection('users')
+      .where('name', '>=', input)
+      .where('name', '<=', input + '\uf8ff')
+      .onSnapshot(snapshot => {
+        const listUsers = [];
+
+        snapshot.forEach(doc => {
+          listUsers.push({
+            ...doc.data(),
+            id: doc.id,
+          });
+        });
+
+        setUsers(listUsers);
+      });
+
+    return () => subscriber();
+  }, [input]);
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -14,7 +42,7 @@ const Search = () => {
           <Input
             placeholder="Procurando por alguém?"
             value={input}
-            onChangeText={text => setInput(text.target.value)}
+            onChangeText={text => setInput(text)}
           />
         </InputArea>
         <List />
--- src/pages/Search/styles.js
+++ src/pages/Search/styles.js
@@ -11,14 +11,14 @@ export const InputArea = styled.View`
   align-items: center;
   background-color: #262626;
   padding: 0 16px;
-  border-radius: 8;
+  border-radius: 8px;
 `;
 
 export const Input = styled.TextInput.attrs({
   placeholderTextColor: '#aaa',
 })`
   flex: 1;
-  margin-left: 8;
+  margin-left: 8px;
   color: #fff;
   font-size: 16px;
 `;
