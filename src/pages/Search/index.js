7843ef03ff80fee70d9604c6fc5d512fb146b9df
MarcosVel <marcos.veloso@sempreceub.com>
2022-09-22 00:20:20 -0300

    feat: search input styles

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Keyboard, TouchableWithoutFeedback } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import { Container, Input, InputArea, List } from './styles';
 
 const Search = () => {
+  const [input, setInput] = useState('');
+
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Container>
+        <InputArea>
+          <Feather name="search" size={20} color="#e52246" />
+          <Input
+            placeholder="Procurando por alguém?"
+            value={input}
+            onChangeText={text => setInput(text.target.value)}
+          />
+        </InputArea>
+        <List />
+      </Container>
+    </TouchableWithoutFeedback>
   );
 };
 
---
+++ src/pages/Search/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  padding: 8px 16px 0;
+  background-color: #353840;
+  flex: 1;
+`;
+
+export const InputArea = styled.View`
+  flex-direction: row;
+  align-items: center;
+  background-color: #262626;
+  padding: 0 16px;
+  border-radius: 8;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#aaa',
+})`
+  flex: 1;
+  margin-left: 8;
+  color: #fff;
+  font-size: 16px;
+`;
+
+export const List = styled.FlatList``;
