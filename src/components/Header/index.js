8bca56cc0d09f960b628718626469da9211fa66f
MarcosVel <marcos.veloso@sempreceub.com>
2022-09-22 22:46:54 -0300

    refactor: add logout button in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import { Container, Red, Title } from './styles';
+import React, { useContext } from 'react';
+import Feather from 'react-native-vector-icons/Feather';
+import { AuthContext } from '../../contexts/auth';
+import { Container, Logout, Red, Title } from './styles';
 
-export default function Header() {
+export default function Header({ hasLogOut }) {
+  const { signOut } = useContext(AuthContext);
+
   return (
     <Container>
       <Title>
         Dev<Red>Post</Red>
       </Title>
+      {hasLogOut && (
+        <Logout
+          onPress={() => signOut()}
+          hitSlop={{
+            top: 20,
+            left: 20,
+            right: 20,
+            bottom: 20,
+          }}>
+          <Feather name="log-out" size={20} color="#fff" />
+        </Logout>
+      )}
     </Container>
   );
 }
